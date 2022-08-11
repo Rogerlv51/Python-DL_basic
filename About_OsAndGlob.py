1cab554a4033842d7562be161af7045d4a1d4752
Rogerlv51 <949608370@qq.com>
2022-08-11 10:00:38 +0800

    glob库更新

--- About_OsAndGlob.py
+++ About_OsAndGlob.py
@@ -1,11 +1,21 @@
 import os
+import glob
+# glob库是利用通配符来匹配找到对应文件的，类似于正则表达式
 
 print("当前目录：", os.getcwd())   # 得到当前目录地址
 
 # 返回指定目录下的所有文件和目录名
-root_dir = os.listdir("C:/Users/EVER/Desktop/Python-DL_basic/")
+root_dir = os.listdir("C:/Users/EVER/Desktop/CodeSpace/Python-DL_basic/")
 print(root_dir)
-origin_path = "C:/Users/EVER/Desktop/Python-DL_basic/"
+origin_path = "C:/Users/EVER/Desktop/CodeSpace/Python-DL_basic/"
+
+# glob.glob返回一个找到指定文件的文件名列表
+os.chdir(origin_path)    # 先进入指定目录
+list = glob.glob('*.ipynb')   # 比如这里我们找到所有的ipynb文件名, glob里面是传入一个参数pathname
+list2 = glob.glob(os.path.join(origin_path + '*.ipynb'))
+print(list)
+print(list2)   # 两者结果一致，但是通过pathname的方式可以返回文件的完整地址，推荐用第二种方法
+
 os.makedirs(origin_path + 'test', exist_ok=True)   # 在指定目录下生成一个新的文件夹（目录）
 
 path = os.path.join(origin_path + "test/")    # 组合目录名生成新的path字符串
